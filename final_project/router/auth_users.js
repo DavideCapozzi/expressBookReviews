2a8bca5b354472fe07284db57a541464136cf7ae
DavideCapozzi <capozzi.davide99@gmail.com>
2025-02-26 14:56:51 -0500

    finished task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,31 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    return users.find(user => user.username === username) !== undefined;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    return !!users.find(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password; 
+
+  if (!username || !password) {
+    return res.status(400).json({message: "missing username or password"})
+  } 
+  else if (!authenticatedUser) {
+    return res.status(401).json({message: "incorrect username or password"})
+  }
+  else {
+    const accessToken = jwt.sign({ data: password }, "access", {
+      expiresIn: 60 * 60,
+    });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).json({ message: "User successfully logged in." });
+  }
 });
 
 // Add a book review
